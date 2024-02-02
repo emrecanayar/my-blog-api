e3b47c76d7c3dfc51d5b87fc88336021dd56341d
Emre Can Ayar <emrecan.ayar@hotmail.com>
2024-02-02 23:29:32 +0300

    Update CreateContactCommandValidator.cs

--- src/projects/myblog/webAPI.Application/Features/Contacts/Commands/Create/CreateContactCommandValidator.cs
+++ src/projects/myblog/webAPI.Application/Features/Contacts/Commands/Create/CreateContactCommandValidator.cs
@@ -6,9 +6,17 @@ public class CreateContactCommandValidator : AbstractValidator<CreateContactComm
 {
     public CreateContactCommandValidator()
     {
-        RuleFor(c => c.FullName).NotEmpty().WithMessage("Ad Soyad alaný boþ olamaz.").MaximumLength(50).WithMessage("Ad Soyad alaný maksiumum 50 karakter olmalýdýr.");
-        RuleFor(c => c.Email).NotEmpty().WithMessage("E-posta alaný boþ olamaz").MaximumLength(50).WithMessage("E-posta alaný maksimum 50 karakter olmalýdýr.").EmailAddress().WithMessage("E-posta formatý hatalýdýr.");
-        RuleFor(c => c.PhoneNumber).NotEmpty().MaximumLength(15).WithMessage("Telefon numarasý maksimum 15 karakter olmalýdýr.");
-        RuleFor(c => c.Message).NotEmpty().MaximumLength(500).WithMessage("Mesaj alaný maksimum 500 karakter olmalýdýr.");
+        RuleFor(c => c.FullName).NotEmpty().WithMessage("Ad Soyad alaný boþ olamaz.")
+                                .MaximumLength(50).WithMessage("Ad Soyad alaný maksiumum 50 karakter olmalýdýr.");
+
+        RuleFor(c => c.Email).NotEmpty().WithMessage("E-posta alaný boþ olamaz")
+                             .MaximumLength(50).WithMessage("E-posta alaný maksimum 50 karakter olmalýdýr.")
+                             .EmailAddress().WithMessage("E-posta formatý hatalýdýr.");
+
+        RuleFor(c => c.PhoneNumber).NotEmpty().WithMessage("Telefon numarasý alaný boþ olamaz.")
+                                   .MaximumLength(15).WithMessage("Telefon numarasý maksimum 15 karakter olmalýdýr.")
+                                   .Matches(@"^\+?[0-9]{1,15}$").WithMessage("Telefon numarasý sadece sayýsal karakterler içermelidir.");
+
+        RuleFor(c => c.Message).NotEmpty().WithMessage("Mesaj alaný boþ olamaz.").MaximumLength(500).WithMessage("Mesaj alaný maksimum 500 karakter olmalýdýr.");
     }
 }
