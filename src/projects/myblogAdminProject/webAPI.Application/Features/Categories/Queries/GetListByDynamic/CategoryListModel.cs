0a13f745b4d45cd25527578e9cd27df00683e9fc
Emre Can Ayar <emrecan.ayar@hotmail.com>
2024-02-20 19:52:40 +0300

    new CategoryService created

---
+++ src/projects/myblogAdminProject/webAPI.Application/Features/Categories/Queries/GetListByDynamic/CategoryListModel.cs
@@ -0,0 +1,15 @@
+﻿using Application.Features.Categories.Queries.GetList;
+using Core.Persistence.Paging;
+
+namespace webAPI.Application.Features.Categories.Queries.GetListByDynamic
+{
+    public class CategoryListModel : BasePageableModel
+    {
+        public IList<GetListCategoryListItemDto> Items { get; set; }
+
+        public CategoryListModel()
+        {
+            Items = default!;
+        }
+    }
+}
---
+++ src/projects/myblogAdminProject/webAPI.Application/Features/Categories/Queries/GetListByDynamic/GetListByDynamicCategoryQuery.cs
@@ -0,0 +1,52 @@
+﻿using Application.Services.Repositories;
+using AutoMapper;
+using Core.Application.Pipelines.Authorization;
+using Core.Application.Requests;
+using Core.Application.ResponseTypes.Concrete;
+using Core.Domain.Entities;
+using Core.Persistence.Dynamic;
+using Core.Persistence.Paging;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+using System.Net;
+using static Application.Features.Categories.Constants.CategoriesOperationClaims;
+
+namespace webAPI.Application.Features.Categories.Queries.GetListByDynamic
+{
+    public class GetListByDynamicCategoryQuery : IRequest<CustomResponseDto<CategoryListModel>>, ISecuredRequest
+    {
+        public PageRequest PageRequest { get; set; }
+        public DynamicQuery? DynamicQuery { get; set; }
+        public string[] Roles => new[] { Admin, Read };
+
+        public GetListByDynamicCategoryQuery()
+        {
+            PageRequest = default!;
+            DynamicQuery = default!;
+        }
+
+        public class GetListByDynamicCategoryQueryHandler : IRequestHandler<GetListByDynamicCategoryQuery, CustomResponseDto<CategoryListModel>>
+        {
+            private readonly ICategoryRepository _categoryRepository;
+            private readonly IMapper _mapper;
+            public GetListByDynamicCategoryQueryHandler(ICategoryRepository categoryRepository, IMapper mapper)
+            {
+                _categoryRepository = categoryRepository;
+                _mapper = mapper;
+            }
+
+            public async Task<CustomResponseDto<CategoryListModel>> Handle(GetListByDynamicCategoryQuery request, CancellationToken cancellationToken)
+            {
+                IPaginate<Category> categories = await _categoryRepository.GetListByDynamicAsync(
+                      dynamic: request.DynamicQuery,
+                      index: request.PageRequest.PageIndex,
+                      size: request.PageRequest.PageSize,
+                      include: i => i.Include(i => i.CategoryUploadedFiles));
+
+                CategoryListModel mappedCategoryListModel = _mapper.Map<CategoryListModel>(categories);
+                return CustomResponseDto<CategoryListModel>.Success((int)HttpStatusCode.OK, mappedCategoryListModel, true);
+            }
+        }
+
+    }
+}
